6781e8f34b64635930ef9a4364574b58fd4cb643
napieraj.malgorzata <napieraj.malgorzata@gmail.com>
2013-10-20 21:19:53 +0000

    Gosia - dopisanie steepesta i greedyego (+generator sąsiedztwa)

--- Zad1/measurement.cpp
+++ Zad1/measurement.cpp
@@ -3,34 +3,23 @@
 void measureAlgorythmPerformance(algorythmFunction aFunc, AlgorythmData* aData, int minDurationInSec, int minRepeats){
 
 	long repeats = 0;
-	int rate = 0;
+	double rate = 0;
 	std::vector<int> result;
+	
 
 	clock_t start = clock();
 
 	do {
 		result = aFunc(aData);
-		rate += rateResult(aData, result);
+		rate = rateResult(aData, result);
+		std::cout <<"Rate: " << rate << std::endl;
 		repeats++;
 
-	} while (clock() - start < minDurationInSec * 1000000 || repeats < minRepeats); 
+	} while (/*clock() - start < minDurationInSec * 1000000 || */repeats < minRepeats); 
 
 	clock_t end = (clock() - start);
 
-	std::cout << "Time total: " << end << ", repeats: " << repeats << std::endl;
-	std::cout << "Time per function: " << (double) end / (double) repeats << std::endl;
-	std::cout << "Avg rate: " << (double) rate / repeats << std::endl; 
+	std::cout << "AvgTime " << (double) end / ((double) repeats) << std::endl;
 
-};
-
-int rateResult(AlgorythmData* aData, std::vector<int> vec) {
-	int rate = 0, lastIdx = vec.size() - 1;
-	
-	for (int i = 0; i < lastIdx; i++) {
-		rate += aData->distances[vec[i]][vec[i + 1]];
-	}
-	rate += (aData->distances[vec[0]][vec[lastIdx]]);
-	
-	return rate;
 };
 
